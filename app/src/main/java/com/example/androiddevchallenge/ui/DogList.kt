9f72b3294f4dc7fc3227930a2b90c924df05557f
jufeng <hiphonezhu@gmail.com>
2021-02-26 11:43:20 +0800

    ./gradlew app:spotlessApply

--- app/src/main/java/com/example/androiddevchallenge/ui/DogList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/DogList.kt
@@ -20,12 +20,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
